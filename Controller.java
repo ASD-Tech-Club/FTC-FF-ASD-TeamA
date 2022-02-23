a9b3d3d7b4fada061ac08004272c9b252a6b463e
Daiigr <danielgurbac@gmail.com>
2022-02-23 05:15:46 -0500

    Copied Working Code from Robot

--- Controller.java
+++ Controller.java
@@ -2,6 +2,9 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.Blinker;
+import com.qualcomm.robotcore.hardware.Gyroscope;
+import com.qualcomm.robotcore.hardware.ColorSensor;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.hardware.DigitalChannel;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
@@ -13,74 +16,81 @@ import com.qualcomm.robotcore.util.ElapsedTime;
 @TeleOp
 
 public class Controller extends LinearOpMode {
-    //motor objects
     private DcMotor leftMotor;
     private DcMotor rightMotor;
     private DcMotor armMotor;
-	private DcMotor tableMotor;
-	private Servo armServo
-
     private float leftMotorPower;
     private float rightMotorPower;
-
     private float MovementXaxis;
     private float MovementYaxis;
     private float ArmYaxis;
-	
-	private boolean armServoActive;
-	private boolean tableMotorActive;
-	
-	
-
+    private Servo Grabber;
+    private DcMotor SpinnerMotor;
+   // boolean GrabberActivate;
 
     @Override
     public void runOpMode() {
     
-   	 leftMotor = hardwareMap.get(DcMotor.class, "LeftDrive");
-   	 rightMotor = hardwareMap.get(DcMotor.class, "RightDrive");
-   	 rmMotor = hardwareMap.get(DcMotor.class, "ArmDrive");
-	 tableMotor = hardwareMap.get(DcMotor.class, "Spinner");
-	 armServo = hardwareMap.get(Servo.class, "Grabber");
-	
-   	 waitForStart();
+        leftMotor = hardwareMap.get(DcMotor.class, "LeftDrive");
+        rightMotor = hardwareMap.get(DcMotor.class, "RightDrive");
+        armMotor = hardwareMap.get(DcMotor.class, "ArmDrive");
+        Useless = hardwareMap.get(DcMotor.class, "Useless");
+        Grabber = hardwareMap.get(Servo.class, "SpinnerMotor");
+        
+        waitForStart();
 
-   	 while (opModeIsActive()) {
+        while (opModeIsActive()) {
+        // current inputs    
+        MovementYaxis = this.gamepad1.left_stick_y;
+        MovementXaxis = this.gamepad1.left_stick_x;
+        ArmYaxis = this.gamepad1.right_stick_y;
 
-    // grabs current value of controller inputs
+        //arm code 
+        armMotor.setPower(ArmYaxis);
+        
+        int ArmPowerIndicator = Math.abs( (int)(ArmYaxis*10));
+        telemetry.addData("Arm:", generateBar(ArmPowerIndicator));
 
-    MovementYaxis = this.gamepad1.left_stick_y;
-   	MovementXaxis = this.gamepad1.left_stick_x;
-    ArmYaxis = this.gamepad1.right_stick_y;
-	armServoActive = this.gamepad1.a;
-	tableMotorActive = this.gamepad1.y;
-   	 
-    // converts Y,X input of left_stick into independent motor control
-   	 leftMotorPower = MovementYaxis - MovementXaxis;
-   	 rightMotorPower = MovementYaxis + MovementXaxis;
+        //movment code
+        leftMotorPower = MovementYaxis + -MovementXaxis;
+        rightMotorPower = MovementYaxis + MovementXaxis;
+        
+        leftMotor.setPower(leftMotorPower);
+        rightMotor.setPower(rightMotorPower);
+        
+        if(gamepad1.a){
+                Grabber.setPosition(0.5);
+            
+        } else{
+            
+            Grabber.setPosition(0);
+        }
+        
+        if(gamepad1.y){
+            Useless.setPower(1);
+            
+        }else{
+            Useless.setPower(0);
 
-  	 //sets motor power
-   	 leftMotor.setPower(leftMotorPower);
-   	 rightMotor.setPower(rightMotorPower);
-  	 armMotor.setPower(ArmYaxis);
-	 
-	 if(tableMotorActive){
-		tableMotor.setPower(1);
-	 }else{
-		 tableMotor.setPower(0);	 
-	 }
-	 
-	 if(armServoActive){
-		 armServo.setPosition(1);
-	 }else{
-		 armServo.setPosition(0);
-		 
-	 }
-	 
+        }
+        
+        
+        int LeftPowerIndicator = Math.abs( (int)(leftMotorPower*10));
+        int RightPowerIndicator = Math.abs((int)(rightMotorPower*10));
+        telemetry.addData("Movement Motor 1: ", generateBar(LeftPowerIndicator));
+       
+    }}
     
-    //displays current power onto tablet
-        	telemetry.addData("Power: ", "Left Motor:" + leftMotorPower*100 + "% Right Motor:" + rightMotorPower*100 + "%" );  
-   		 telemetry.update();
-   		 }
+    public String generateBar(int power){ // power bar code
+        
+            String output = "";
+            for(int i = 0; i < power ;i++){
+            output += "█";
+            }
+        return output;
+            
+        
+        
+        
     }
-    
 }
